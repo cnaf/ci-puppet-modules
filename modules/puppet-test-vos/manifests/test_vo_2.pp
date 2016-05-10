b75d93bf0fc5956a96ab44dd235dca5cfb68062f
Enrico Vianello <maverick17586@gmail.com>
2016-05-10 12:59:37 +0200

    Updated test.vo.2 ca dn

--- modules/puppet-test-vos/manifests/test_vo_2.pp
+++ modules/puppet-test-vos/manifests/test_vo_2.pp
@@ -6,7 +6,7 @@ class puppet-test-vos::test_vo_2 {
       { server => 'vgrid02.cnaf.infn.it',
       port => '15001',
       dn => '/C=IT/O=INFN/OU=Host/L=CNAF/CN=vgrid02.cnaf.infn.it',
-      ca_dn => '/C=IT/O=INFN/CN=INFN CA'
+      ca_dn => '/C=IT/O=INFN/CN=INFN Certification Authority'
       }
     ]
   }
