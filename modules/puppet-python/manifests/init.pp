96b3b9d17386ef229f2724ebf1729879f7a6a677
Marco Caberletti <marco.caberletti@gmail.com>
2016-05-12 14:28:46 +0200

    Add EL7 case.

--- modules/puppet-python/manifests/init.pp
+++ modules/puppet-python/manifests/init.pp
@@ -3,16 +3,22 @@ class puppet-python {
   Exec {
     path => ['/bin', '/usr/bin']
   }
-  
+
   case $lsbmajdistrelease {
-   
+
     5: {
         $python_packages = ["python26", "python26-distribute"]
         $easy_install = "easy_install-2.6"
         $pip = "pip2.6"
         }
-   
+
     6:  {
+        $python_packages = ["python", "python-setuptools"]
+        $easy_install = "easy_install"
+        $pip = "pip"
+        }
+
+    7:  {
         $python_packages = ["python", "python-setuptools"]
         $easy_install = "easy_install"
         $pip = "pip"
