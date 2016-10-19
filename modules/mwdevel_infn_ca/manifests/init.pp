4e6caa39486e3ea82368456ab2124c7cf6e34c4e
Marco Caberletti <marco.caberletti@gmail.com>
2016-10-19 09:08:21 +0200

    Update metadata

--- modules/mwdevel_infn_ca/manifests/init.pp
+++ modules/mwdevel_infn_ca/manifests/init.pp
@@ -1,7 +1,7 @@
 class mwdevel_infn_ca {
   include mwdevel_egi_trust_anchors
 
-  package { "ca_INFN-CA-*":
+  package { 'ca_INFN-CA-*':
     ensure  => latest,
     require => Class['mwdevel_egi_trust_anchors'],
   }
--- modules/mwdevel_test_vos/manifests/init.pp
+++ modules/mwdevel_test_vos/manifests/init.pp
@@ -1,4 +1,7 @@
 class mwdevel_test_vos {
-  include mwdevel_test_vos::bane, mwdevel_test_vos::testers, mwdevel_test_vos::test_vo, mwdevel_test_vos::test_vo_2, 
-  mwdevel_test_vos::dteam
+  include mwdevel_test_vos::bane
+  include mwdevel_test_vos::testers
+  include mwdevel_test_vos::test_vo
+  include mwdevel_test_vos::test_vo_2
+  include mwdevel_test_vos::dteam
 }
