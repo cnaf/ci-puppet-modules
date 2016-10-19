c79f43d1a69a218f666fa06e581a8ac780dfd72b
Marco Caberletti <marco.caberletti@gmail.com>
2016-10-19 08:46:18 +0200

    Change dependency. Cosmetic fixes.

--- modules/mwdevel_umd_repo/manifests/init.pp
+++ modules/mwdevel_umd_repo/manifests/init.pp
@@ -1,42 +1,43 @@
 class mwdevel_umd_repo ($umd_repo_version = 3,) {
   case $umd_repo_version {
-    3 : {
+    3       : {
       case $lsbmajdistrelease {
-        6 : {
+        6       : {
           $umd_repo = 'http://repository.egi.eu/sw/production/umd/3/sl6/x86_64/updates/umd-release-3.14.2-1.el6.noarch.rpm'
           $umd_repo_install_cmd = "yum install -y ${umd_repo}"
-          $umd_repo_file = "/etc/yum.repos.d/UMD-3-base.repo"
+          $umd_repo_file = '/etc/yum.repos.d/UMD-3-base.repo'
         }
         default : {
-          fail("Unsupported platform for UMD-3")
+          fail('Unsupported platform for UMD-3')
         }
       }
     }
-    4: {
+    4       : {
       case $lsbmajdistrelease {
-        6 : {
+        6       : {
           $umd_repo = 'http://repository.egi.eu/sw/production/umd/4/sl6/x86_64/updates/umd-release-4.1.1-1.el6.noarch.rpm'
           $umd_repo_install_cmd = "yum install -y ${umd_repo}"
-          $umd_repo_file = "/etc/yum.repos.d/UMD-4-base.repo"
+          $umd_repo_file = '/etc/yum.repos.d/UMD-4-base.repo'
         }
 
-        7 : {
+        7       : {
           $umd_repo = 'http://repository.egi.eu/sw/production/umd/4/centos7/x86_64/updates/umd-release-4.1.1-1.el7.noarch.rpm'
           $umd_repo_install_cmd = "yum install -y ${umd_repo}"
-          $umd_repo_file = "/etc/yum.repos.d/UMD-4-base.repo"
+          $umd_repo_file = '/etc/yum.repos.d/UMD-4-base.repo'
 
         }
         default : {
-          fail("Unsupported platform for UMD-4")
+          fail('Unsupported platform for UMD-4')
         }
       }
     }
-    default :  {
+    default : {
       fail("Unsupported umd repo version: ${umd_repo_version}")
     }
   }
 
-  Exec { path => '/bin:/sbin:/usr/bin:/usr/sbin', }
+  Exec {
+    path => '/bin:/sbin:/usr/bin:/usr/sbin', }
 
   exec { 'umd-repo':
     command => $umd_repo_install_cmd,
