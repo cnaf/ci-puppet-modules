80f64b370850181853305119f702d5671d587d51
enricovianello <maverick17586@gmail.com>
2017-08-29 09:29:31 +0200

    Added version parameter to gpfs module
    GPFS-Repo file linked to multiversion repository

---
+++ modules/mwdevel_gpfs/examples/versioned.pp
@@ -0,0 +1,3 @@
+class { 'mwdevel_gpfs':
+        version => '4.1.1-14',
+}
--- modules/mwdevel_gpfs/manifests/init.pp
+++ modules/mwdevel_gpfs/manifests/init.pp
@@ -1,10 +1,10 @@
-class mwdevel_gpfs {
+class mwdevel_gpfs($version = "3.4.0-17") {
   require mwdevel_gpfs_repo
   require mwdevel_base_build_env
 
   $gpfs_packages = ['gpfs.base', 'gpfs.docs', 'gpfs.gpl', 'gpfs.msg.en_US']
 
-  package { $gpfs_packages: ensure => latest, }
+  package { $gpfs_packages: ensure => $version, }
 
   file { '/etc/profile.d/gpfs_env.sh':
     content => 'export PATH=$PATH:/usr/lpp/mmfs/bin/',
