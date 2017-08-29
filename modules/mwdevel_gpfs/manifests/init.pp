edef7d455d07ce52070f4c1e7b803fcbf89c4e5f
enricovianello <maverick17586@gmail.com>
2017-08-29 16:14:18 +0200

    Used regex to manage different lists of gpfs packages dep on version

--- modules/mwdevel_gpfs/manifests/init.pp
+++ modules/mwdevel_gpfs/manifests/init.pp
@@ -2,12 +2,24 @@ class mwdevel_gpfs($version = "3.4.0-17") {
   require mwdevel_gpfs_repo
   require mwdevel_base_build_env
 
-  $gpfs_packages = $version ? {
-    "4.1.1-14", "4.2.0-3", "latest" => ['gpfs.base', 'gpfs.docs', 'gpfs.gpl', 'gpfs.msg.en_US', 'gpfs.gskit', 'gpfs.ext'],
-    default => ['gpfs.base', 'gpfs.docs', 'gpfs.gpl', 'gpfs.msg.en_US']
-  }
+  if $version =~ /^4./ {
 
-  package { $gpfs_packages: ensure => $version, }
+    $gpfs_packages = ['gpfs.base', 'gpfs.docs', 'gpfs.gpl', 'gpfs.msg.en_US', 'gpfs.ext']
+    package {
+        $gpfs_packages: ensure => $version
+    }
+
+    package {
+        'gpfs.gskit': ensure => '8.0.50-47'
+    }
+
+  } else {
+
+    $gpfs_packages = ['gpfs.base', 'gpfs.docs', 'gpfs.gpl', 'gpfs.msg.en_US']
+    package {
+        $gpfs_packages: ensure => $version
+    }
+  }
 
   file { '/etc/profile.d/gpfs_env.sh':
     content => 'export PATH=$PATH:/usr/lpp/mmfs/bin/',
