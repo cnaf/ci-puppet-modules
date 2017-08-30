e74a8c2f42c6a9fca62b898d6c011f99e1d048ff
enricovianello <maverick17586@gmail.com>
2017-08-30 15:33:27 +0200

    Added package specific configuration for GPFS 4.2.3-4

--- modules/mwdevel_gpfs/manifests/init.pp
+++ modules/mwdevel_gpfs/manifests/init.pp
@@ -2,22 +2,29 @@ class mwdevel_gpfs($version = "3.4.0-17") {
   require mwdevel_gpfs_repo
   require mwdevel_base_build_env
 
-  if $version =~ /^4./ {
+  if $version =~ /^3./ {
+
+    $gpfs_packages = ['gpfs.base', 'gpfs.docs', 'gpfs.gpl', 'gpfs.msg.en_US']
+    package {
+        $gpfs_packages: ensure => $version
+    }
+
+  } else {
 
     $gpfs_packages = ['gpfs.base', 'gpfs.docs', 'gpfs.gpl', 'gpfs.msg.en_US', 'gpfs.ext']
     package {
         $gpfs_packages: ensure => $version
     }
 
-    package {
-        'gpfs.gskit': ensure => '8.0.50-47'
+    if $version == "4.2.0-3" {
+      package {
+          'gpfs.gskit': ensure => '8.0.50-47'
+      }
     }
-
-  } else {
-
-    $gpfs_packages = ['gpfs.base', 'gpfs.docs', 'gpfs.gpl', 'gpfs.msg.en_US']
-    package {
-        $gpfs_packages: ensure => $version
+    if $version == "4.2.3-4" {
+      package {
+          'gpfs.gskit': ensure => '8.0.50-50'
+      }
     }
   }
 
