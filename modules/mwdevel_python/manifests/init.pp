c1285b3add9dc5cddd878414a10b9456f9ae6842
Marco Caberletti <marco.caberletti@gmail.com>
2017-10-30 10:32:32 +0100

    Removed epel-release package install

--- modules/mwdevel_python/manifests/init.pp
+++ modules/mwdevel_python/manifests/init.pp
@@ -1,12 +1,8 @@
 class mwdevel_python {
   $python_packages = ['python', 'python-setuptools', 'python-pip']
 
-  package {
-    'epel-release':
-      ensure => present,;
-
-    $python_packages:
-      ensure  => present,
-      require => Package['epel-release'],
+  package { $python_packages:
+    ensure  => present,
+    require => Package['epel-release'],;
   }
 }
