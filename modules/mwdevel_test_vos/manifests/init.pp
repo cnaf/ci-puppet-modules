9bbd3a3a0f6e2a6f13677928f3d819d37fcf180f
enricovianello <maverick17586@gmail.com>
2019-08-01 16:44:57 +0200

    Removed not used test vos

--- modules/mwdevel_test_vos/manifests/init.pp
+++ modules/mwdevel_test_vos/manifests/init.pp
@@ -1,6 +1,4 @@
 class mwdevel_test_vos {
-  include mwdevel_test_vos::bane
-  include mwdevel_test_vos::testers
   include mwdevel_test_vos::test_vo
   include mwdevel_test_vos::test_vo_2
   include mwdevel_test_vos::dteam
