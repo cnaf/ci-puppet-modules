8a944c1ae3118863def66f1579f8cc0243e7eb89
enricovianello <maverick17586@gmail.com>
2020-01-07 17:10:44 +0100

    Updated test.vo* dn and ca_dn

--- modules/mwdevel_test_vos/manifests/test_vo.pp
+++ modules/mwdevel_test_vos/manifests/test_vo.pp
@@ -7,8 +7,8 @@ class mwdevel_test_vos::test_vo {
     servers => [{
         server => 'vgrid02.cnaf.infn.it',
         port   => '15000',
-        dn     => '/C=IT/O=INFN/OU=Host/L=CNAF/CN=vgrid02.cnaf.infn.it',
-        ca_dn  => '/C=IT/O=INFN/CN=INFN Certification Authority',
+        dn     => '/DC=org/DC=terena/DC=tcs/C=IT/ST=Lazio/L=Frascati/O=Istituto Nazionale di Fisica Nucleare/CN=vgrid02.cnaf.infn.it',
+        ca_dn  => '/C=NL/ST=Noord-Holland/L=Amsterdam/O=TERENA/CN=TERENA eScience SSL CA 3',
       }
     ],
   }
