2ba92d6519f7bf79d9e262b100fe14b596956ace
enricovianello <maverick17586@gmail.com>
2020-03-26 17:03:30 +0100

    WIP: added mwdevel_test_vos2

---
+++ modules/mwdevel_test_vos2/manifests/init.pp
@@ -0,0 +1,28 @@
+# @summary configure all the test VOs used into our testbeds
+#
+class mwdevel_test_vos2 {
+
+  class { 'voms::dteam': }
+
+  voms::client { 'test.vo':
+    servers => [
+      {
+        server => 'vgrid02.cnaf.infn.it',
+        port   => '15000',
+        dn     => '/DC=org/DC=terena/DC=tcs/C=IT/ST=Lazio/L=Frascati/O=Istituto Nazionale di Fisica Nucleare/CN=vgrid02.cnaf.infn.it',
+        ca_dn  => '/C=NL/ST=Noord-Holland/L=Amsterdam/O=TERENA/CN=TERENA eScience SSL CA 3',
+      },
+    ],
+  }
+
+  voms::client { 'test.vo.2':
+    servers => [
+      {
+        server => 'vgrid02.cnaf.infn.it',
+        port   => '15001',
+        dn     => '/DC=org/DC=terena/DC=tcs/C=IT/ST=Lazio/L=Frascati/O=Istituto Nazionale di Fisica Nucleare/CN=vgrid02.cnaf.infn.it',
+        ca_dn  => '/C=NL/ST=Noord-Holland/L=Amsterdam/O=TERENA/CN=TERENA eScience SSL CA 3',
+      },
+    ],
+  }
+}
